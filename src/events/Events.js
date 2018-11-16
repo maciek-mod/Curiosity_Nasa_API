437b82e221111e83789d15e693c198a6314e16e6
Maciej Modliński <maciej.modlinski@veneo.eu>
2018-11-16 13:05:05 +0100

    add popup and css

--- src/events/Events.js
+++ src/events/Events.js
@@ -26,20 +26,49 @@ class Events extends React.Component {
         }
     }
 
+    showFull(el){
+        let img_src = el.target.src,
+            popup = document.getElementsByClassName("popup")[0],
+            img_container = document.getElementsByClassName("img_container")[0],
+            element = img_container.getElementsByTagName("IMG")[0],
+            img_width = el.target.clientWidth,
+            img_height = el.target.clientHeight,
+            close = document.getElementsByClassName("close")[0];
+
+        if (img_height < img_width) {
+            popup.classList.add("width");
+        }
+
+        element.src = img_src;
+        // img_container.appendChild(element);
+        popup.classList.add("show");
+
+        close.addEventListener("click", function(event){
+            // var delete_element = img_container.getElementsByTagName("IMG")[0];
+            // console.log(delete_element);
+            popup.classList.remove("show");
+            // img_container.removeChild(delete_element);
+            element.src = "";
+            if (popup.classList.contains("width")) {
+                popup.classList.remove("width");
+            }
+        });
+    }
+
     render() {
         if (this.props.eventsStore.data !== null) {
             if (this.props.eventsStore.data.length > 0) {
                 return (
-                    <div className="photo">
+                    <div className="container">
                         <EventFilter goDate={this.goDate.bind(this)} />
                         <h2>Sol: {this.props.eventsStore.data[0].sol}</h2>
                         <h2>Earth day: {this.props.eventsStore.data[0].earth_date}</h2>
                         {
                             this.props.eventsStore.data.map(item => {
                                 return (
-                                    <div key={item.id} >
+                                    <div key={item.id} className="photoContainer">
                                         <p>Camera: {item.camera.full_name}</p>
-                                        <img src={item.img_src}/>
+                                        <img onClick={this.showFull.bind(this)} src={item.img_src}/>
                                     </div>
                                 )
                             })
